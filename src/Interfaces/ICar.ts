a97f150828b8e5367474cb0d6d189fe1ba559d3a
Samuelr499 <samuelribeiro2@hotmail.com>
2022-11-17 21:07:24 -0300

    cria interface ICar

---
+++ src/Interfaces/ICar.ts
@@ -0,0 +1,10 @@
+export default interface ICar {
+  model: string,
+  year: number,
+  color: string,
+  status?: boolean,
+  buyValue: number,
+  doorsQty: number,
+  seatsQty: number,
+  id?: string,
+}
