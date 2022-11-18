ad6c8e69a5ee49cb53730f83f586830aa18d9165
Samuelr499 <samuelribeiro2@hotmail.com>
2022-11-18 19:12:54 -0300

    adiciona mock de cars

---
+++ tests/unit/Services/carsMocks.ts
@@ -0,0 +1,31 @@
+export const carsArray = [
+  {
+    id: '6377f1d31817dff63d3faa2b',
+    model: 'Marea',
+    year: 2002,
+    color: 'Black',
+    status: true,
+    buyValue: 15.990,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+  {
+    id: 'a377f5d31857dff63d3faa2b',
+    model: 'Tempra',
+    year: 1995,
+    color: 'Black',
+    buyValue: 39.000,
+    doorsQty: 2,
+    seatsQty: 5,
+  },
+];
+
+export const carMock = {
+  id: 'a377f5d31857dff63d3faa2b',
+  model: 'Tempra',
+  year: 1995,
+  color: 'Black',
+  buyValue: 39.000,
+  doorsQty: 2,
+  seatsQty: 5,
+};
