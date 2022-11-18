0e21d692a851892786ce729a001acfcba279b8c1
Samuelr499 <samuelribeiro2@hotmail.com>
2022-11-18 13:05:38 -0300

    model find e findOne para rota get

--- src/Models/CarODM.ts
+++ src/Models/CarODM.ts
@@ -12,20 +12,30 @@ class Car {
   private model: Model<ICar>;
 
   constructor() {
-    this.schema = new Schema<ICar>({
-      model: { type: String, required: true },
-      year: { type: Number, required: true },
-      color: { type: String, required: true },
-      status: { type: Boolean, required: false },
-      buyValue: { type: Number, required: true },
-      doorsQty: { type: Number, required: true },
-      seatsQty: { type: Number, required: true },
-    });
+    this.schema = new Schema<ICar>(
+      {
+        model: { type: String, required: true },
+        year: { type: Number, required: true },
+        color: { type: String, required: true },
+        status: { type: Boolean, required: false },
+        buyValue: { type: Number, required: true },
+        doorsQty: { type: Number, required: true },
+        seatsQty: { type: Number, required: true },
+      },
+    );
     this.model = models.Car || model('Car', this.schema);
   }
 
   public async create(car: ICar): Promise<ICar> {
     return this.model.create({ ...car });
+  }
+
+  public async getCars(): Promise<ICar[]> {
+    return this.model.find({});
+  }
+
+  public async getById(id: string): Promise<ICar | null> {
+    return this.model.findOne({ _id: id });
   }
 }
 
