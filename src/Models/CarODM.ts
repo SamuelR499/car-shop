38bc7d3ddcc99fb68b4f75356479a42da7c41696
Samuelr499 <samuelribeiro2@hotmail.com>
2022-11-19 09:08:42 -0300

    muda tipo da ODM update para void

--- src/Models/CarODM.ts
+++ src/Models/CarODM.ts
@@ -37,8 +37,8 @@ class Car {
   public async getById(id: string): Promise<ICar | null> {
     return this.model.findOne({ _id: id });
   }
-  public async update(id: string, car: ICar): Promise<ICar | null> {
-    return this.model.updateOne({ _id: id }, { ...car }) as any;
+  public async update(id: string, car: ICar): Promise<void> {
+    this.model.updateOne({ _id: id }, { ...car });
   }
 }
 
