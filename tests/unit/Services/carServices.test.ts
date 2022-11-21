585053749cacc2002e98e00b44512dabbb8aad11
Samuelr499 <samuelribeiro2@hotmail.com>
2022-11-21 19:47:26 -0300

    teste para camada service da rota post /cars

--- tests/unit/Services/carServices.test.ts
+++ tests/unit/Services/carServices.test.ts
@@ -4,10 +4,13 @@ import { Model } from 'mongoose';
 import ICar from '../../../src/Interfaces/ICar';
 import CarService from '../../../src/Services/CarService';
 import Car from '../../../src/Domains/Car';
-import { carsArray, carMock } from './carsMocks';
+import { carsArray, carMock, updateMock } from './carsMocks';
 
-describe('testando a service de carService', function () {
+describe('Testando a service de carService', function () {
   describe('teste para a rota post "/cars"', function () {
+    afterEach(function () {
+      sinon.restore();
+    });
     it('deve criar um car com SUCESSO', async function () {
       const carInput: ICar = {
         model: 'tesla',
@@ -34,10 +37,28 @@ describe('testando a service de carService', function () {
   
       expect(result).to.be.deep.equal(carOutput);
     });
+
+    it('Se nao for preenchido os campo retorna null', async function () {
+      const carInput: ICar = {
+        model: 'tesla',
+        year: 2030,
+        color: 'Branco',
+        status: true,
+        buyValue: 20.0,
+        doorsQty: 4,
+        seatsQty: 2,
+      };
+    
+      sinon.stub(Model, 'create').resolves(false);
+      const service = new CarService();
+      const result = await service.addCar(carInput);
+  
+      expect(result).to.be.equal(null);
+    });
   });
 
-  describe('teste para a rota get "/cars"', function () {
-    it('deveria trazer todos os carros em um array de objetos', async function () {
+  describe('Teste para a rota get "/cars"', function () {
+    it('Deveria trazer todos os carros em um array de objetos', async function () {
       const carList = carsArray.map((car) => new Car(car));
       sinon.stub(Model, 'find').resolves(carList);
       const service = new CarService();
@@ -46,8 +67,8 @@ describe('testando a service de carService', function () {
     });
   });
 
-  describe('teste para a rota get "/cars/:id"', function () {
-    it('Em caso de sucesso deveria trazer um carro especifico pelp id', async function () {
+  describe('Teste para a rota get "/cars/:id"', function () {
+    it('Em caso de sucesso devera atualizar e retornar o objeto atualizado', async function () {
       const car = new Car(carMock);
       sinon.stub(Model, 'findOne').resolves(carMock);
       const service = new CarService();
@@ -55,16 +76,81 @@ describe('testando a service de carService', function () {
       expect(result).to.be.deep.equal(car);
     });
 
-    it(' em caso de falha Deveria lançar uma exceção quando o id é inválida', async function () {
-      sinon.stub(Model, 'findOne').resolves({});
+    it('Quando o id é inválida, deveria lançar um erro "Invalid mongo id"', async function () {
+      sinon.stub(Model, 'findOne').resolves(null);
 
       try {
         const service = new CarService();
-        await service.getById('invalid31857dff63d3faa2b');
+        await service.getById('menosDe24Car');
       } catch (error) {
-        expect((error as Error).message).not.to.be.equal('Invalid Key!');
+        expect((error as Error).message).to.be.equal('Invalid mongo id');
       }
     });
+
+    it('Em caso de id incorreto Deveria lançar um erro "Car not found"', async function () {
+      sinon.stub(Model, 'findOne').resolves(null);
+
+      try {
+        const service = new CarService();
+        await service.getById('esteIdIvalidoDeveDarErro');
+      } catch (error) {
+        expect((error as Error).message).to.be.equal('Car not found');
+      }
+    });
+
+    afterEach(function () {
+      sinon.restore();
+    });
+  });
+
+  describe('Teste para a rota update "/cars/:id"', function () {
+    it('Em caso de sucesso deveria trazer o novo carro especifico pelo id', async function () {
+      const car = new Car(carMock);
+      const service = new CarService();
+
+      sinon.stub(Model, 'updateOne').resolves(updateMock);
+      sinon.stub(Model, 'findOne').resolves(carMock);
+      const result = await service.update(carMock.id, carMock);
+      expect(result).to.be.deep.equal(car);
+    });
+
+    it('Quando o id é inválida, deveria lançar um erro "Invalid mongo id"', async function () {
+      sinon.stub(Model, 'findOne').resolves(null);
+      try {
+        const service = new CarService();
+        await service.update('menosDe24Car', carMock);
+      } catch (error) {
+        expect((error as Error).message).to.be.equal('Invalid mongo id');
+      }
+    });
+
+    it('Em caso de id incorreto Deveria lançar um erro "Car not found"', async function () {
+      sinon.stub(Model, 'updateOne').resolves({
+        acknowledged: true,
+        modifiedCount: 0,
+        upsertedId: '637bf2bd65e9c62bf204c7d1' as any,
+        upsertedCount: 0,
+        matchedCount: 0,
+      });
+      sinon.stub(Model, 'findOne').resolves(null);
+      const carInput: ICar = {
+        model: 'tesla',
+        year: 2030,
+        color: 'Branco',
+        status: true,
+        buyValue: 20.0,
+        doorsQty: 4,
+        seatsQty: 2,
+      };
+
+      try {
+        const service = new CarService();
+        await service.update('637bf2bd65e9c62bf204c7d1', carInput);
+      } catch (error) {
+        expect((error as Error).message).to.be.equal('Car not found');
+      }
+    });
+
     afterEach(function () {
       sinon.restore();
     });
