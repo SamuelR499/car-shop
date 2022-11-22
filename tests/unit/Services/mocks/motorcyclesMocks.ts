52b3da4340ce6946b1a725f85f0541bd344abef9
Samuelr499 <samuelribeiro2@hotmail.com>
2022-11-22 11:03:39 -0300

    cria pasta mocks

---
+++ tests/unit/Services/mocks/motorcyclesMocks.ts
@@ -0,0 +1,41 @@
+export const motorcyclesArray = [
+  {
+    id: '634852326b35b59438fbea2f',
+    model: 'Honda Cb 600f Hornet',
+    year: 2005,
+    color: 'Yellow',
+    status: true,
+    buyValue: 30.000,
+    category: 'Street',
+    engineCapacity: 600,
+  },
+  {
+    id: '634852326b35b59438fbea31',
+    model: 'Honda Cbr 1000rr',
+    year: 2011,
+    color: 'Orange',
+    status: true,
+    buyValue: 59.900,
+    category: 'Street',
+    engineCapacity: 1000,
+  },
+];
+
+export const motorcycleMock = {
+  id: '634852326b35b59438fbea2f',
+  model: 'Honda Cb 600f Hornet',
+  year: 2005,
+  color: 'Yellow',
+  status: true,
+  buyValue: 30.000,
+  category: 'Street',
+  engineCapacity: 600,
+};
+
+export const updateMock = {
+  acknowledged: true,
+  modifiedCount: 0,
+  upsertedId: 'a377f5d31857dff63d3faa2b' as any,
+  upsertedCount: 0,
+  matchedCount: 1,
+};
